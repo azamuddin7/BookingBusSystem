2b6068e0ac04e86ce05142fc4b783351338065eb
azamuddin7 <azamhasni7@gmail.com>
2018-01-04 01:30:40 +0800

    betulkan error

--- src/java/bean/User.java
+++ src/java/bean/User.java
@@ -87,7 +87,7 @@ public class User implements Serializable {
         return email;
     }
     
-    public void setEmail(String Email) {
+    public void setEmail(String email) {
         this.email = email;
     }
 }
