46d9396083c379d0fee1d31e2815071054e8df04
IRSYAD KAMIL <34701021+mikr96@users.noreply.github.com>
2018-01-05 01:28:00 +0800

    + insert database tapi ada minor error. Nanti repair

--- src/java/user/UserPayment.java
+++ src/java/user/UserPayment.java
@@ -5,19 +5,52 @@
  */
 package user;
 
+import bean.PaymentDetails;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import jdbc.JDBCUtility;
 
 /**
  *
  * @author irsya
  */
+
+@WebServlet(name = "UserPayment", urlPatterns = {"/UserPayment"})
 public class UserPayment extends HttpServlet {
 
+    private JDBCUtility jdbcUtility;
+    private Connection con;
+    
+    public void init() throws ServletException
+    {
+        String driver = "com.mysql.jdbc.Driver";
+
+        String dbName = "bus";
+        String url = "jdbc:mysql://localhost/" + dbName + "?";
+        String userName = "root";
+        String password = "";
+
+        jdbcUtility = new JDBCUtility(driver,
+                                      url,
+                                      userName,
+                                      password);
+
+        jdbcUtility.jdbcConnect();
+        con = jdbcUtility.jdbcGetConnection();
+    } 
+    
+    
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -27,14 +60,56 @@ public class UserPayment extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+ protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            response.sendRedirect(request.getContextPath() + "/UserPayment.jsp");
+        
+        //get form data from VIEW > V-I
+        String cardNo = request.getParameter("cardNo");
+        String cardName = request.getParameter("cardName");
+        String cardExpiry = request.getParameter("cardExpiry");
+        String cardCVV = request.getParameter("cardCVV");
+        
+        String sqlInsert = "INSERT INTO paymentdetails(cardNo, cardName, cardExpiry, cardCVV) VALUES(?, ?, ?, ?)"; 
+        
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement(sqlInsert);
+            preparedStatement.setString(1, cardNo);
+            preparedStatement.setString(2, cardName);
+            preparedStatement.setString(3, cardExpiry);
+            preparedStatement.setString(4, cardCVV);
+            preparedStatement.executeUpdate();
+                
+                PaymentDetails pd = new PaymentDetails();
+                pd.setCardNo(cardNo);
+                pd.setCardName(cardName);
+                pd.setCardExpiry(cardExpiry);
+                pd.setCardCVV(cardCVV);
+                
+        HttpSession session = request.getSession();
+        session.setAttribute("paymentdetails", pd);
+        response.sendRedirect(request.getContextPath() + "/UserPayment.jsp");
+                
+        }
+        catch (SQLException ex) {  
+            PrintWriter out = response.getWriter();
+            out.println(ex);    
         }
     }
+ 
+        void sendPage(HttpServletRequest req, HttpServletResponse res, String fileName) throws ServletException, IOException
+    {
+        // Get the dispatcher; it gets the main page to the user
+	RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(fileName);
+
+	if (dispatcher == null)
+	{
+            System.out.println("There was no dispatcher");
+	    // No dispatcher means the html file could not be found.
+	    res.sendError(res.SC_NO_CONTENT);
+	}
+	else
+	    dispatcher.forward(req, res);
+    }  
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -74,5 +149,4 @@ public class UserPayment extends HttpServlet {
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
-
 }
